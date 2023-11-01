3dc8d99adb793506e54adc1df5e5522ec0632b23
Joel Kingsley Rangarajan <joel.kingsley.rangarajan@iem.fraunhofer.de>
2023-11-01 20:40:18 +0100

    Removed unused login view methods

--- home/views.py
+++ home/views.py
@@ -20,31 +20,11 @@ def register(request):
         customerProfileRegisterForm = CustomerProfileRegisterForm()
         return render(request, 'accounts/register.html', {'userRegisterForm': userRegisterForm, 'customerProfileRegisterForm': customerProfileRegisterForm})
 
-def user_login(request):
-    if request.method == 'POST':
-        ust_id = request.POST['ust_id']
-        password = request.POST['password']
-
-        # Authenticate the user
-        user = authenticate(request, username=ust_id, password=password)
-
-        if user is not None:
-            login(request, user)  # Log in the user
-            return redirect('dashboard')  # Redirect to the dashboard page after successful login
-        else:
-            # Authentication failed, handle the error (e.g., show an error message)
-            pass
-
-    return render(request, 'accounts/login.html')
-
 # Create your views here.
 
 def index(request):
     # Page from the theme 
     return render(request, 'pages/index.html')
-
-def login(request):
-    return render(request, 'accounts/login.html')
 
 def purchase(request):
     return render(request, 'pages/purchase.html')
