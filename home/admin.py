d2ab4b053a1719a97981ddeeabed5c54cd710b17
Lokesha Reddy <shashank.lokesha@iem.fraunhofer.de>
2023-11-07 18:05:16 +0100

    Customizable CSS

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomerProfile, Akkuvariante, Kabelvariante, Schnittstelle, Color, UILabel, Image
+from .models import CustomerProfile, Akkuvariante, Kabelvariante, Schnittstelle, Color, UILabel, Image, Order
 
 admin.site.register(CustomerProfile)
 admin.site.register(Akkuvariante)
@@ -8,3 +8,4 @@ admin.site.register(Schnittstelle)
 admin.site.register(Color)
 admin.site.register(UILabel)
 admin.site.register(Image)
+admin.site.register(Order)
