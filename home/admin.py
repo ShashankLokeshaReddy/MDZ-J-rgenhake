c7afbccd878d1556dd876187b2aa8a7318343b73
Lokesha Reddy <shashank.lokesha@iem.fraunhofer.de>
2023-11-07 13:44:18 +0100

    Customizable dropdown values

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import CustomerProfile
+from .models import CustomerProfile, Akkuvariante, Kabelvariante, Schnittstelle, Color, UILabel, Image
 
 admin.site.register(CustomerProfile)
+admin.site.register(Akkuvariante)
+admin.site.register(Kabelvariante)
+admin.site.register(Schnittstelle)
+admin.site.register(Color)
+admin.site.register(UILabel)
+admin.site.register(Image)
--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('purchase/', views.purchase, name='purchase'),
+    path('customerProfile/', views.get_customer_profiles, name='get_customer_profiles'),
+    path('akkuvarianten/', views.get_akkuvarianten, name='get_akkuvarianten'),
+    path('kabelvarianten/', views.get_kabelvarianten, name='get_kabelvarianten'),
+    path('schnittstellen/', views.get_schnittstellen, name='get_schnittstellen'),
+    path('colors/', views.get_colors, name='get_colors'),
+    path('ui-labels/', views.get_ui_labels, name='get_ui_labels'),
+    path('images/', views.get_image_path, name='get_image_path'),
 ]
