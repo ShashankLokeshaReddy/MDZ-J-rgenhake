dc1ebfe5281de9a00c7fb95131936eb713f5d8db
Joel Kingsley Rangarajan <joel.kingsley.rangarajan@iem.fraunhofer.de>
2023-11-30 13:28:28 +0100

    Added customer profile page, added pillow, crispy forms and bootstrap

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
     path("admin/", admin.site.urls),
     path("", include('admin_material.urls'))
 ]
+
+# TODO: For production usage visit https://docs.djangoproject.com/en/3.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development for common deployment strategies
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
